09d2245ae2b79ec6f635b9ab242fea5908066b47
Xelba91 <a_lbert_o@hotmail.com>
2024-01-22 16:42:14 +0100

    ricetta

--- D1.js
+++ D1.js
@@ -120,4 +120,58 @@ console.log("Esercizio 7: jhon è uguale a Jhon?",name1 === name2) ;
 // EXTRA
 console.log("Esercizio 7 Extra: trasformato i valori con il .toLowerCase",name1.toLowerCase() === name2.toLowerCase()) ;
 
+//Esercizio Ricetta
 
+let ricetta = 'La besciamella è una salsa di base tra le più versatili in cucina: facile e veloce da realizzare, è perfetta per dare una marcia in più a tanti piatti diversi, dalle lasagne alla pasta al forno, come quella gratinata con i finocchi per esempio, fino addirittura agli spinaci o al petto di pollo! La sua paternità è rivendicata tanto dai francesi quanto dagli italiani. L’ipotesi più accreditata è che sia stata Caterina de’ Medici ad averla importata in Francia con il nome che aveva allora, ovvero “salsa colla”. Ma è grazie al cortigiano Louis de Béchameil che prese poi il nome di salsa besciamella nonostante Pellegrino Artusi preferisse italianizzarla nel suo ricettario con il nome di “balsamella”, ormai in disuso. Scoprite tutti i segreti per ottenere una besciamella perfetta fatta in casa cremosa e senza grumi e scommettiamo che non ricorrerete mai più a quella confezionata!'
+
+console.log(ricetta)
+
+// Ingredienti
+let ingredienti = "Latte = 1lt\nFarina= 100gr\nBurro=100gr\nSale=2gr\nNoce Moscata=10gr"
+console.log(ingredienti)
+let latte = 1000;
+let farina = 100;
+let burro = 100;
+let sale= 2;
+let noceMoscata = 10;
+let cottura  = 0;
+let cottura2 = 0;
+
+
+//Preparazione
+
+// COME PREPARARE LA BESCIAMELLA
+
+console.log("1 - Versate il latte in un pentolino")
+let pentolino = 0;
+pentolino += latte;
+console.log("2 - aggiungete la noce moscata")
+pentolino += noceMoscata;
+console.log("3 - aggiungere e il sale, poi fatelo scaldare.")
+pentolino += sale;
+cottura = 3;
+console.log("Il peso del primo pentolino è:",pentolino, "grammi\ntempo di cottura :", cottura,"minuti")
+console.log("4 - Mettete il burro in un altro pentolino  e fatelo sciogliere a fuoco dolce.");
+let pentolino2 = 0;
+pentolino2 += burro;
+cottura2 = 2;
+console.log("5 - Poi aggiungete la farina in una volta sola.");
+console.log("6 - Mescolate vigorosamente con una frusta a mano per evitare la formazione di grumi.") 
+console.log("7 - Continuate a mescolare fino ad ottenere un composto dorato, il roux.") 
+pentolino2 += farina;
+console.log("Il peso del secondo pentolino è:",pentolino2, "grammi")
+console.log("tempo di cottura:",cottura2,"minuti")
+console.log("8 - A questo punto stemperate il roux versando prima poco latte caldo e poi tutto il resto."); 
+console.log("9 - Continuate a mescolare a fuoco dolce fino a raggiungere la consistenza desiderata; cuocendo la besciamella per 5-6 minuti otterrete una densità media. La vostra besciamella è pronta per essere utilizzata!");
+let besciamella = 0;
+let cotturaTot = (cottura + cottura2) + 6;
+besciamella = pentolino + pentolino2;
+let aa;
+let bi;
+let ci;
+aa = "Il peso totale della ricetta per 4 persone è:"+ besciamella +" grammi.\n";
+bi = "La porzione per una persona è di "+ besciamella / 4 + " grammi.\n";
+ci = "Il tempo totale di cottura è: "+ cotturaTot + " minuti";
+
+let risposta = aa+bi+ci;
+console.log(risposta);
